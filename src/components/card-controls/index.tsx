d8961b313de3c48889c2343813daf95e0d5a6471
Trista (tbmueller) <trista.mueller@gmail.com>
2025-04-13 09:43:03 -0400

    Add dropdown, no state

--- src/components/card-controls/index.tsx
+++ src/components/card-controls/index.tsx
@@ -1,11 +1,18 @@
 import { Card, Input } from "pixel-retroui";
 import { useStore } from "../../store/hooks";
+import { TypeDropdown } from "./type-dropdown";
 export const CardControls = () => {
     const { name, setName } = useStore();
     if (!setName) return null;
 
     return <Card className="flex flex-col gap-2">
-        <label htmlFor="name">Name</label>
-        <Input id="name" placeholder="Pikachu" value={name} onChange={(e) => setName(e.target.value)} />
+        <div className="flex flex-col gap-1">
+            <label htmlFor="name">Name</label>
+            <Input id="name" placeholder="Pikachu" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div className="flex flex-col gap-1">
+            <label>Type</label>
+            <TypeDropdown className="w-full" />
+        </div>
     </Card>
 };
