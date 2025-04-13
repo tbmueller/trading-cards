e454f9eb097f3404efb94fc2997c5bd26056b398
Trista (tbmueller) <trista.mueller@gmail.com>
2025-04-12 20:50:13 -0400

    More components, some layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@
 import '@react95/core/GlobalStyle';
 import '@react95/core/themes/win95.css';
 import { CardControls } from './components/card-controls'
+import { Flex } from 'react-flex-lite';
 
 function App() {
   return (
+    <Flex hcenter vcenter w="100vw">
       <CardControls />
+    </Flex>
   )
 }
 
