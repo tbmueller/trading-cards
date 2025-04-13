aef078bc472d754e54fbcbaf03a748c5eaada65c
Trista (tbmueller) <31867918+tbmueller@users.noreply.github.com>
2025-04-12 21:08:09 -0400

    Add Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // @ts-expect-error This is the correct syntax for importing this
 import '@react95/core/GlobalStyle';
 import '@react95/core/themes/win95.css';
+import "tailwindcss";
 import { CardControls } from './components/card-controls'
 import { Card } from './components/card';
 
