6ff3b643e8fdff7370f85b9f8accb2224ddb5199
Trista (tbmueller) <trista.mueller@gmail.com>
2025-04-12 23:04:16 -0400

    Update card view layout

--- src/components/card-view/index.tsx
+++ src/components/card-view/index.tsx
@@ -6,9 +6,9 @@ import { downloadImage } from "./download-image";
 export const CardView = () => {
   const cardRef = useRef<HTMLDivElement>(null);
   return (
-    <>
+    <div className="flex flex-col gap-2">
       <TradingCard ref={cardRef} />
       <Button onClick={() => downloadImage(cardRef)}>Download card</Button>
-    </>
+    </div>
   )
 }
