84f1f1791087089339ea7204dc2ffd5f50d99d12
Trista (tbmueller) <31867918+tbmueller@users.noreply.github.com>
2025-04-12 20:38:13 -0400

    Add styling, first component

---
+++ src/components/card-controls/index.tsx
@@ -0,0 +1,14 @@
+import { Frame } from "@react95/core";
+
+export const CardControls = () => {
+  return (
+    <Frame
+        bgColor="$material"
+        boxShadow="$out"
+        h="100px"
+        w="200px"
+    >
+        Hello world
+    </Frame>
+  );
+};
