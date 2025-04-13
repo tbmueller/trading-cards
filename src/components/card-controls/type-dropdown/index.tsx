7f989fb3458276c47062ce864603fd4e0e6feb7f
Trista (tbmueller) <trista.mueller@gmail.com>
2025-04-13 10:20:02 -0400

    Options

--- src/components/card-controls/type-dropdown/index.tsx
+++ src/components/card-controls/type-dropdown/index.tsx
@@ -1,18 +1,27 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "pixel-retroui";
 import { CARD_STYLES } from "../../trading-card/card-styles";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { CardType } from "../../../types/card-type";
 import { useStore } from "../../../store/hooks";
-
+import { JSX, useMemo } from "react";
+import { Option } from "./option";
 export const TypeDropdown = ({ className }: { className?: string }) => {
   const { type, setType } = useStore();
+
+  // TODO: make this nicer
+  const options: Record<CardType, JSX.Element> = useMemo(() => ({
+    fire: <Option type="fire" />,
+    water: <Option type="water" />,
+    grass: <Option type="grass" />,
+    flying: <Option type="flying" />,
+  }), []);
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className={className}>{type}</DropdownMenuTrigger>
+      <DropdownMenuTrigger className={className}>{options[type]}</DropdownMenuTrigger>
       <DropdownMenuContent className="w-full">
-        {Object.keys(CARD_STYLES).map((type: string) => (
-          <div key={type} className="w-full" onClick={() => setType(type as CardType)}>
-            <DropdownMenuItem className="w-full"><FontAwesomeIcon icon={CARD_STYLES[type as CardType].icon} /> {type}</DropdownMenuItem>
+        {Object.keys(CARD_STYLES).map((typeKey: string) => (
+          <div key={typeKey} className="w-full" onClick={() => setType(typeKey as CardType)}>
+            <DropdownMenuItem className="w-full">{options[typeKey as CardType]}</DropdownMenuItem>
           </div>
         ))}
       </DropdownMenuContent>
