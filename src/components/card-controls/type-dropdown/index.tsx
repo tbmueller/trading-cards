0bb04062b3e7d0b59d79395e24a2ab01a166cde5
Trista (tbmueller) <31867918+tbmueller@users.noreply.github.com>
2025-04-13 09:53:33 -0400

    Add state for type

--- src/components/card-controls/type-dropdown/index.tsx
+++ src/components/card-controls/type-dropdown/index.tsx
@@ -2,14 +2,18 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import { CARD_STYLES } from "../../trading-card/card-styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { CardType } from "../../../types/card-type";
+import { useStore } from "../../../store/hooks";
 
 export const TypeDropdown = ({ className }: { className?: string }) => {
+  const { type, setType } = useStore();
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className={className}>Select type</DropdownMenuTrigger>
+      <DropdownMenuTrigger className={className}>{type}</DropdownMenuTrigger>
       <DropdownMenuContent className="w-full">
         {Object.keys(CARD_STYLES).map((type: string) => (
-          <DropdownMenuItem key={type} className="w-full"><FontAwesomeIcon icon={CARD_STYLES[type as CardType].icon} /> {type}</DropdownMenuItem>
+          <div key={type} className="w-full" onClick={() => setType(type as CardType)}>
+            <DropdownMenuItem className="w-full"><FontAwesomeIcon icon={CARD_STYLES[type as CardType].icon} /> {type}</DropdownMenuItem>
+          </div>
         ))}
       </DropdownMenuContent>
     </DropdownMenu>
--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,13 +1,16 @@
 import { create } from "zustand";
 import { combine } from "zustand/middleware";
+import { CardType } from "../types/card-type";
 
 export const useStore = create(
   combine(
     {
       name: "",
+      type: "fire" as CardType,
     },
     (set) => ({
       setName: (name: string) => set({ name }),
+      setType: (type: CardType) => set({ type }),
     })
   )
 );
