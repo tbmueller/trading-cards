793c5c4c06471a5bbeb6126a3436ed24d9f21bbc
Trista (tbmueller) <trista.mueller@gmail.com>
2025-04-12 21:39:01 -0400

    Remove React95 :'(

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-// @ts-expect-error This is the correct syntax for importing this
-import '@react95/core/GlobalStyle';
-import '@react95/core/themes/win95.css';
-import "tailwindcss";
 import { CardControls } from './components/card-controls'
 import { Card } from './components/card';
 
 function App() {
   return (
-    <>
-      <CardControls />
-      <Card/>
-    </>
+    <div className="flex justify-center w-screen">
+      <div className="flex gap-2">
+        <CardControls />
+        <Card/>
+      </div>
+    </div>
   )
 }
 
--- src/components/card-controls/index.tsx
+++ src/components/card-controls/index.tsx
@@ -1,14 +1,3 @@
-import { Frame } from "@react95/core";
-
 export const CardControls = () => {
-  return (
-    <Frame
-        bgColor="$material"
-        boxShadow="$out"
-        h="100px"
-        w="200px"
-    >
-        Hello world
-    </Frame>
-  );
+  return "Card Controls"
 };
