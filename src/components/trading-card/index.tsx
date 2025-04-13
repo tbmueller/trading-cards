6650d2dd8618e20d10206be64dbfa6367ad5bc76
Trista (tbmueller) <trista.mueller@gmail.com>
2025-04-12 23:03:03 -0400

    Add card download

--- src/components/trading-card/index.tsx
+++ src/components/trading-card/index.tsx
@@ -1,12 +1,18 @@
 import { NameContext } from "../../contexts/name";
 import { useContext } from "react";
 
-export const TradingCard = () => {
+interface TradingCardProps {
+    ref: React.RefObject<HTMLDivElement | null>;
+}
+
+export const TradingCard = ({ ref }: TradingCardProps) => {
     const { name } = useContext(NameContext);
     return (
-        <div className="border-yellow-500 border-4 border-solid rounded-md bg-white min-h-[400px] min-w-[300px]">
-            {name}
-            hello
+        <div ref={ref} className="bg-transparent">
+            <div className="border-yellow-500 border-4 border-solid rounded-md bg-white min-h-[400px] min-w-[300px]">
+                {name}
+                hello
+            </div>
         </div>
     );
 };
