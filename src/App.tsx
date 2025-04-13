6ec86f5ef8567733672a6414218ae5b24e6af192
Trista (tbmueller) <trista.mueller@gmail.com>
2025-04-12 20:38:13 -0400

    Add styling, first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// @ts-expect-error This is the correct syntax for importing this
+import '@react95/core/GlobalStyle';
+import '@react95/core/themes/win95.css';
+import { CardControls } from './components/card-controls'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <CardControls />
   )
 }
 
