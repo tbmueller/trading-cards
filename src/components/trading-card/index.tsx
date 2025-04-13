3fe3403879891e77109e6fbd774de1adcfed14e9
Trista (tbmueller) <trista.mueller@gmail.com>
2025-04-13 09:53:33 -0400

    Add state for type

--- src/components/trading-card/index.tsx
+++ src/components/trading-card/index.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { gradientClassName } from "../../util/radial-gradient-classname";
+import { gradientClassName } from "../../util/gradient-classname";
 import { CARD_STYLES } from "./card-styles";
 import { useStore } from "../../store/hooks";
 
@@ -8,12 +8,12 @@ interface TradingCardProps {
 }
 
 export const TradingCard = ({ ref }: TradingCardProps) => {
-    const { name } = useStore();
-    const type = "fire";
+    const { name, type } = useStore();
     const { icon, iconColor } = CARD_STYLES[type];
+    const gradientClass = gradientClassName(type);
     return (
         <div ref={ref}>
-            <div className={`flex border-yellow-500 border-4 border-solid rounded-md min-h-[400px] min-w-[300px] ${gradientClassName(type)}`}>
+            <div className={`flex border-yellow-500 border-4 border-solid rounded-md min-h-[400px] min-w-[300px] ${gradientClass}`}>
                 <div className="flex border-white border-solid border-2 rounded-sm min-h-full min-w-full">
                     <div className="m-2 p-2 h-min w-full flex gap-2 items-center bg-zinc-200 rounded-md">
                         <FontAwesomeIcon icon={icon} className={iconColor} />
