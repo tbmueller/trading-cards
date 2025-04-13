1cfc3ad837dff03489b299943427da6020c201c7
Trista (tbmueller) <trista.mueller@gmail.com>
2025-04-12 22:04:29 -0400

    Step 1: name

--- src/components/card-controls/index.tsx
+++ src/components/card-controls/index.tsx
@@ -1,7 +1,11 @@
-import { Card } from "pixel-retroui";
-
+import { Card, Input } from "pixel-retroui";
+import { NameContext } from "../../contexts/name";
+import { useContext } from "react";
 export const CardControls = () => {
-  return <Card>
-    Hello
-  </Card>
+    const { name, setName } = useContext(NameContext);
+    if (!setName) return null;
+
+    return <Card>
+        <Input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+    </Card>
 };
