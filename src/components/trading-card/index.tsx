2b354dbbd8866bce7381b48dcb2978297daec427
Trista (tbmueller) <31867918+tbmueller@users.noreply.github.com>
2025-04-12 22:32:14 -0400

    Visual changes

--- src/components/trading-card/index.tsx
+++ src/components/trading-card/index.tsx
@@ -4,8 +4,9 @@ import { useContext } from "react";
 export const TradingCard = () => {
     const { name } = useContext(NameContext);
     return (
-        <div className="border-5 border-yellow-500 rounded-md">
+        <div className="border-yellow-500 border-4 border-solid rounded-md bg-white min-h-[400px] min-w-[300px]">
             {name}
+            hello
         </div>
     );
 };
